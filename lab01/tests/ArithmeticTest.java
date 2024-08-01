051a15382c23cbc34e03ba0ae0131a555f1e96a1
killerwhaleskadi <112710549+killerwhaleskadi@users.noreply.github.com>
2024-08-01 17:40:00 +0800

    Setup

--- lab01/tests/ArithmeticTest.java
+++ lab01/tests/ArithmeticTest.java
@@ -10,27 +10,27 @@ import static com.google.common.truth.Truth.assertThat;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ArithmeticTest {
 
-    /** Performs a few arbitrary tests to see if the product method is
-     * correct */
-    @Test
-    @Order(0)
-    @DisplayName("Test product correctness")
-    public void testProduct() {
-        assertThat(Arithmetic.product(5, 6)).isEqualTo(30);
-        assertThat(Arithmetic.product(5, -6)).isEqualTo(-30);
-        assertThat(Arithmetic.product(0, -6)).isEqualTo(0);
-        assertThat(Arithmetic.product(-5, -6)).isEqualTo(30);
-    }
+	/** Performs a few arbitrary tests to see if the product method is
+	 * correct */
+	@Test
+	@Order(0)
+	@DisplayName("Test product correctness")
+	public void testProduct() {
+		assertThat(Arithmetic.product(5, 6)).isEqualTo(30);
+		assertThat(Arithmetic.product(5, -6)).isEqualTo(-30);
+		assertThat(Arithmetic.product(0, -6)).isEqualTo(0);
+		assertThat(Arithmetic.product(-5, -6)).isEqualTo(30);
+	}
 
-    /** Performs a few arbitrary tests to see if the sum method is correct */
-    @Test
-    @Order(1)
-    @DisplayName("Test sum correctness")
-    public void testSum() {
-        assertThat(Arithmetic.sum(5, 6)).isEqualTo(11);
-        assertThat(Arithmetic.sum(5, -6)).isEqualTo(-1);
-        assertThat(Arithmetic.sum(0, -6)).isEqualTo(-6);
-        assertThat(Arithmetic.sum(6, -6)).isEqualTo(0);
-        assertThat(Arithmetic.sum(-5, -5)).isEqualTo(-10);
-    }
+	/** Performs a few arbitrary tests to see if the sum method is correct */
+	@Test
+	@Order(1)
+	@DisplayName("Test sum correctness")
+	public void testSum() {
+		assertThat(Arithmetic.sum(5, 6)).isEqualTo(11);
+		assertThat(Arithmetic.sum(5, -6)).isEqualTo(-1);
+		assertThat(Arithmetic.sum(0, -6)).isEqualTo(-6);
+		assertThat(Arithmetic.sum(6, -6)).isEqualTo(0);
+		assertThat(Arithmetic.sum(-5, -5)).isEqualTo(-10);
+	}
 }
